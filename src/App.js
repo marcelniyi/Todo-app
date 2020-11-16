d13f0dfc558a3bdd89afbdaff4b75e09aafc6af4
maxtechR <niyitegekamarcel@gmail.com>
2020-11-16 10:30:17 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,104 +1,24 @@
-import React from 'react';
-import './App.css';
-import firebase from './firebase';
+import React from 'react'
+import Todo from './components/Todo/Todo'
+import SignIn from './components/Login'
+import Register from './components/Register'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import history from './history';
 
 
 class App extends React.Component {
-  constructor(props){
-    super(props);
-      this.state = {
-        todoList:[],
-        activeItem:{
-          id:null,
-          title:'',
-          completed:false,
-        },
-        editing:false,
-      }
-      this.handleSubmit = this.handleSubmit.bind(this)
-  };
-
-
-  componentWillMount(){
-    this.fetchTasks()
-  }
-
-  fetchTasks = async () =>{
-    const todos = await firebase.firestore().collection('todos').get();
-    const result = todos.docs.map((item) => item.data());
-
-    this.setState({
-      todoList:result
-    })
-
-
-  }
-
-  handleSubmit(e){
-    e.preventDefault();
-    firebase.firestore().collection('todos').add({
-      title: "hello there",
-      completed: false
-    })
-};
-
-
-
   render(){
-    var tasks = this.state.todoList
-    var self = this
     return(
-        <div className="container">
 
-          <div id="task-container">
-              <div  id="form-wrapper">
-                 <form onSubmit={this.handleSubmit}  id="form">
-                    <div className="flex-wrapper">
-                        <div style={{flex: 6}}>
-                            <input onChange={this.handleChange} className="form-control" id="title" value={this.state.activeItem.title} type="text" name="title" placeholder="Add task.." />
-                         </div>
-
-                         <div style={{flex: 1}}>
-                            <input id="submit" value="Add to list" className="btn btn-warning" type="submit" name="Add" />
-                          </div>
-                      </div>
-                </form>
-
-              </div>
-
-              <div  id="list-wrapper">
-                    {tasks.map(function(task, index){
-                      return(
-                          <div key={index} className="task-wrapper flex-wrapper">
-
-                            <div onClick={() => self.strikeUnstrike(task)} style={{flex:7}}>
-
-                                {task.completed == false ? (
-                                    <span>{task.title}</span>
-
-                                  ) : (
-
-                                    <strike>{task.title}</strike>
-                                  )}
-
-                            </div>
-
-                            <div style={{flex:1}}>
-                                <button onClick={() => self.startEdit(task)} className="btn btn-sm btn-outline-info">Edit</button>
-                            </div>
-
-                            <div style={{flex:1}}>
-                                <button onClick={() => self.deleteItem(task)} className="btn btn-sm btn-outline-dark delete">-</button>
-                            </div>
-
-                          </div>
-                        )
-                    })}
-              </div>
-          </div>
-
-        </div>
-      )
+        <Router history={history}>
+          <Switch>
+            <Route exact path="/" component={SignIn}/>
+            <Route exact path="/register" component={Register}/>
+            <Route exact path="/home" component={Todo}/>
+          </Switch>
+        </Router>
+      
+    )
   }
 }
 
